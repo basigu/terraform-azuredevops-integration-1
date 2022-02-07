8092ea6e279d3d5e7c73908a643c663b7b40e6a3
basigu <basi_gu@hotmail.com>
2022-02-07 15:03:22 -0800

    Update variables.tf

--- terraform-manifests/aks-cluster/variables.tf
+++ terraform-manifests/aks-cluster/variables.tf
@@ -23,8 +23,8 @@ variable aks {
   description = "AKS properties"
   type = map
   default = {
-    resource_group  = "default-value",
-    cluster_name    = "default-value",
+    resource_group  = "k8s-gb",
+    cluster_name    = "k8s-gb,
     node_pool_name  = "agentpool",
     type            = "VirtualMachineScaleSets"
     identity_type   = "SystemAssigned",
@@ -99,7 +99,7 @@ variable "rm-customers-api-deployment" {
 
 variable "proj_id" {
   description = "Identifier postfix for various resources"
-  default = "default-value"
+  default = "gb"
 }
 
 ##############################################################################################################
